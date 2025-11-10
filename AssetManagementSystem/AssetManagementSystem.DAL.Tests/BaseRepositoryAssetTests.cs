7e1b6710da5fe6efca12c581aed1a3610ee300aa
koval <kovalartem9010@gmail.com>
2025-11-10 23:36:17 +0200

    Add unit tests for Asset BaseRepository and UnitOfWork

---
+++ AssetManagementSystem/AssetManagementSystem.DAL.Tests/BaseRepositoryAssetTests.cs
@@ -0,0 +1,130 @@
+﻿using AssetManagementSystem.DAL.Entities;
+using AssetManagementSystem.DAL.Enums;
+using AssetManagementSystem.DAL.Excetions;
+using AssetManagementSystem.DAL.Repositories.Impl;
+
+namespace AssetManagementSystem.DAL.Tests;
+
+public class BaseRepositoryAssetTests
+{
+    [Fact]
+    public void Create_ShouldAddEntity()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        var repo = new AssetRepositoryAsset(ctx);
+        var id = Guid.NewGuid();
+        var asset = new Asset
+            { Id = id, Name = "Gen", Category = "Equip", Location = "A-101", Status = AssetStatus.Working };
+
+        repo.Create(asset);
+        repo.SaveChanges();
+
+        var saved = ctx.Assets.Find(id);
+        Assert.NotNull(saved);
+        Assert.Equal("Gen", saved.Name);
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnAll()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        ctx.Assets.AddRange(
+            new Asset
+            {
+                Id = Guid.NewGuid(), Name = "Gen1", Category = "Equip1", Location = "A-101",
+                Status = AssetStatus.Working
+            }
+        );
+        ctx.SaveChanges();
+
+        var repo = new AssetRepositoryAsset(ctx);
+        var all = repo.GetAll();
+
+        Assert.Single(all);
+    }
+
+    [Fact]
+    public void GetById_ShouldReturn_WhenExists()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        var id = Guid.NewGuid();
+        ctx.Assets.Add(new Asset
+            { Id = id, Name = "A1", Category = "Equip", Location = "A-101", Status = AssetStatus.Working });
+        ctx.SaveChanges();
+
+        var repo = new AssetRepositoryAsset(ctx);
+        var entity = repo.GetById(id);
+
+        Assert.Equal(id, entity.Id);
+    }
+
+    [Fact]
+    public void GetById_ShouldThrow_WhenNotFound()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        var repo = new AssetRepositoryAsset(ctx);
+
+        Assert.Throws<NotFoundException>(() => repo.GetById(Guid.NewGuid()));
+    }
+
+    [Fact]
+    public void Update_ShouldModifyEntity()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        var id = Guid.NewGuid();
+        ctx.Assets.Add(new Asset
+            { Id = id, Name = "Old", Category = "Equip", Location = "A-101", Status = AssetStatus.Working });
+        ctx.SaveChanges();
+
+        var repo = new AssetRepositoryAsset(ctx);
+        var entity = repo.GetById(id);
+        entity.Name = "New";
+        repo.Update(entity);
+        repo.SaveChanges();
+
+        Assert.Equal("New", ctx.Assets.Find(id)!.Name);
+    }
+
+    [Fact]
+    public void Delete_ById_ShouldRemoveEntity()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        var id = Guid.NewGuid();
+        ctx.Assets.Add(new Asset
+            { Id = id, Name = "Gen", Category = "Equip", Location = "A-101", Status = AssetStatus.Working });
+        ctx.SaveChanges();
+
+        var repo = new AssetRepositoryAsset(ctx);
+        repo.Delete(id);
+        repo.SaveChanges();
+
+        Assert.Null(ctx.Assets.Find(id));
+    }
+
+    [Fact]
+    public void Find_ShouldFilterAndPage()
+    {
+        using var ctx = InMemoryDbFactory.Create();
+        ctx.Assets.AddRange(
+            new Asset
+            {
+                Id = Guid.NewGuid(), Name = "A1", Category = "Equip1", Location = "A-101", Status = AssetStatus.Working
+            },
+            new Asset
+            {
+                Id = Guid.NewGuid(), Name = "A2", Category = "Equip1", Location = "A-101", Status = AssetStatus.Working
+            },
+            new Asset
+            {
+                Id = Guid.NewGuid(), Name = "A3", Category = "Equip1", Location = "A-101", Status = AssetStatus.Working
+            }
+        );
+        ctx.SaveChanges();
+
+        var repo = new AssetRepositoryAsset(ctx);
+        var page = repo.Find(a => a.Name.StartsWith("A"), pageNumber: 1, pageSize: 1).ToList();
+
+        Assert.Single(page);
+        Assert.Equal("A2", page[0].Name);
+    }
+}
---
+++ AssetManagementSystem/AssetManagementSystem.DAL.Tests/UnitOfWorkTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace AssetManagementSystem.DAL.Tests;
+
+public class UnitOfWorkTests
+{
+    private static ApplicationDbContext MakeContextMock(out Mock<ApplicationDbContext> ctxMock)
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+
+        ctxMock = new Mock<ApplicationDbContext>(options) { CallBase = true };
+        return ctxMock.Object;
+    }
+
+    [Fact]
+    public void Repositories_ShouldBeCachedPerUoW()
+    {
+        var ctx = MakeContextMock(out _);
+        var uow = new UnitOfWork(ctx);
+
+        var r1 = uow.Assets;
+        var r2 = uow.Assets;
+
+        Assert.Same(r1, r2);
+    }
+
+    [Fact]
+    public void SaveChanges_ShouldCallContext()
+    {
+        var ctx = MakeContextMock(out var mock);
+        var uow = new UnitOfWork(ctx);
+
+        uow.SaveChanges();
+
+        mock.Verify(c => c.SaveChanges(), Times.Once);
+    }
+
+    [Fact]
+    public async Task SaveChangesAsync_ShouldCallContextAsync()
+    {
+        var ctx = MakeContextMock(out var mock);
+        var uow = new UnitOfWork(ctx);
+
+        await uow.SaveChangesAsync();
+
+        mock.Verify(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
